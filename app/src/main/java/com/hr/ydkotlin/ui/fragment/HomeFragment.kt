4636484074495c907a9fb129dca2d52dc31d6e17
2402zmybie <2402368990@qq.com>
2021-03-04 22:26:48 +0800

    homefragment begin

--- app/src/main/java/com/hr/ydkotlin/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/hr/ydkotlin/ui/fragment/HomeFragment.kt
@@ -4,15 +4,20 @@ import android.graphics.Color
 import android.view.Gravity
 import android.view.View
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.hr.ydkotlin.R
 import com.hr.ydkotlin.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment :BaseFragment() {
     override fun initView(): View? {
-        val tv = TextView(context)
-        tv.gravity = Gravity.CENTER
-        tv.setTextColor(Color.RED)
-        tv.text = javaClass.simpleName;
-        return tv;
+        return View.inflate(context, R.layout.fragment_home,null);
+    }
+
+    override fun initListener() {
+        //初始化recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        //适配
     }
 
 }
